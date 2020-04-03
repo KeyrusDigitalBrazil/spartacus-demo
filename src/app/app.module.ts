b4fb812bbee9a3353a1d0ac80db1248e8a003a3c
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2020-04-02 23:46:05 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://localhost:9002',
+          prefix: '/rest/v2/',
+          legacy: false
+        }
+      },
+      authentication: {
+            client_id: 'mobile_android',
+            client_secret: 'secret'
+          },
+      context: {
+        baseSite: ['electronics-spa']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '1.5',
+        anonymousConsents: true
+      }
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
